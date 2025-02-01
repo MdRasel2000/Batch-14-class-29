5f378f47f77f011abd62f7aeac6fd33bf2f562e4
MdRasel2000 <ctgmdraselislam@gmail.com>
2025-02-01 17:22:49 +0600

    js loop & function

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+let marks = [ 78, 87, 98, 54]
+
+// for(i=0; i<marks.length; i++){
+
+//     console.log(marks[i]);
+// }
+
+
+// let j= 0;
+
+// while(j<=6){
+
+//     console.log("hello world");
+//     j++;
+// }
+
+// let k = 0;
+// while(k<marks.length){
+//        console.log(marks[k]);
+//        k++; 
+// }
+
+
+
+// let w = 0;
+
+// do{
+//     console.log(marks[w]);
+//     w++;
+// }
+
+// while(w<marks.length);
+
+
+// marks.forEach(element=>{
+//     console.log(element);
+// })
+
+
+
+// function show(){
+//     let marks2 = [ 98, 87, 98, 54]
+//     for(i=0; i<marks2.length; i++){
+//       console.log(marks2[i]);
+//  }
+// }
+// show()
+
+
+function sum(number1,number2,number3){
+// let  x=number1;
+// let y=number2;
+// let z=number3;
+
+// let result=x+y+z;
+let result=number1+number2+number3;
+
+console.log(result);
+
+}
+sum (45,45,30);
+
+
+
+function sum(number1,number2,number3){
+    
+    let result=number1+number2+number3;
+    
+    return result;
+    }
+
+     sumattion1 =sum (30,40,30);
+
+     sum2= sumattion1+70;
+
+    console.log(sum2);
